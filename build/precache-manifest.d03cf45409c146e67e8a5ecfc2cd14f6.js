e8515f92bed70fcec081e8949804745c01dd20c1
maulik.j.bhalala <maulik.j.bhalala@doyenhub.com>
2020-07-22 10:57:34 +0530

    new build added

---
+++ build/precache-manifest.d03cf45409c146e67e8a5ecfc2cd14f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e850727b72b5015b06f9d7ba7bd0bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f7eaebecee43a950b65",
+    "url": "/static/css/2.14feee9d.chunk.css"
+  },
+  {
+    "revision": "9712b0fdebb3d038a187",
+    "url": "/static/css/main.88a9aa61.chunk.css"
+  },
+  {
+    "revision": "9f7eaebecee43a950b65",
+    "url": "/static/js/2.eb9968a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eb9968a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9712b0fdebb3d038a187",
+    "url": "/static/js/main.e37f020c.chunk.js"
+  },
+  {
+    "revision": "f2401c040861091065be",
+    "url": "/static/js/runtime-main.66f6ec9d.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
